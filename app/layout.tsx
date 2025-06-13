25e0db5cd648a9a55c72403cab8a9562da4b9baa
ashutosh017 <wwwashutosh225@gmail.com>
2025-06-13 20:49:49 +0530

    fix(navbar): navbar is visible on mobile

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
           >
-            <MobileHeader/>
+            {/* <MobileHeader/> */}
           <Header/>
 
             {children}
