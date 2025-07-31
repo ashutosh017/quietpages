72dd95f24a339a02feb2e5f4a15a20657d568d28
ashutosh017 <wwwashutosh225@gmail.com>
2025-07-31 11:34:11 +0530

    add: sorting in fetching blogs

--- app/blogs/all-blogs.tsx
+++ app/blogs/all-blogs.tsx
@@ -6,7 +6,11 @@ export const revalidate = 60;
 
 export default async function AllBlogs() {
 
-  const blogs = await prisma.blog.findMany();
+  const blogs = await prisma.blog.findMany({
+    orderBy:{
+      dateCreated:"desc"
+    }
+  });
 
   return (
     <>
